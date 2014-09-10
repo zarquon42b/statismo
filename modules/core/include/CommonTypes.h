93ceea74d71b970a027069bfa84e4c78de53b478
Arnaud Gelas <arnaudgelas@gmail.com>
2014-09-10 15:09:31 +0200

    export stuff from CommonTypes

--- modules/core/include/CommonTypes.h
+++ modules/core/include/CommonTypes.h
@@ -55,10 +55,10 @@
 namespace statismo {
 
 
-const double PI	=	3.14159265358979323846;
+STATISMO_CORE_EXPORT const double PI	=	3.14159265358979323846;
 
 /// the type that is used for all vector and matrices throughout the library.
-typedef float ScalarType;
+STATISMO_CORE_EXPORT typedef float ScalarType;
 
 // wrapper struct that allows us to easily select matrix and vectors of an arbitrary
 // type, wich has the same traits as the standard matrix / vector traits
@@ -69,40 +69,40 @@ template <typename TScalar> struct GenericEigenType {
 	typedef Eigen::Matrix<TScalar, 1 , Eigen::Dynamic> RowVectorType;
 
 };
-typedef GenericEigenType<ScalarType>::MatrixType MatrixType;
-typedef GenericEigenType<double>::MatrixType MatrixTypeDoublePrecision;
-typedef GenericEigenType<ScalarType>::DiagMatrixType DiagMatrixType;
-typedef GenericEigenType<ScalarType>::VectorType VectorType;
-typedef GenericEigenType<double>::VectorType VectorTypeDoublePrecision;
-typedef GenericEigenType<ScalarType>::RowVectorType RowVectorType;
+STATISMO_CORE_EXPORT typedef GenericEigenType<ScalarType>::MatrixType MatrixType;
+STATISMO_CORE_EXPORT typedef GenericEigenType<double>::MatrixType MatrixTypeDoublePrecision;
+STATISMO_CORE_EXPORT typedef GenericEigenType<ScalarType>::DiagMatrixType DiagMatrixType;
+STATISMO_CORE_EXPORT typedef GenericEigenType<ScalarType>::VectorType VectorType;
+STATISMO_CORE_EXPORT typedef GenericEigenType<double>::VectorType VectorTypeDoublePrecision;
+STATISMO_CORE_EXPORT typedef GenericEigenType<ScalarType>::RowVectorType RowVectorType;
 
 // type definitions used in the standard file format.
 // Note that these are the same as used by VTK
-const static unsigned Void = 0; // not capitalized, as windows defines: #define VOID void, which causes trouble
-const static unsigned SIGNED_CHAR = 2;
-const static unsigned UNSIGNED_CHAR  = 3;
-const static unsigned SIGNED_SHORT     =  4;
-const static unsigned UNSIGNED_SHORT = 5;
-const static unsigned SIGNED_INT             = 6;
-const static unsigned UNSIGNED_INT   = 7;
-const static unsigned SIGNED_LONG          =  8;
-const static unsigned UNSIGNED_LONG  = 9;
-const static unsigned FLOAT =         10;
-const static unsigned DOUBLE      =   11;
+STATISMO_CORE_EXPORT const static unsigned Void = 0; // not capitalized, as windows defines: #define VOID void, which causes trouble
+STATISMO_CORE_EXPORT const static unsigned SIGNED_CHAR = 2;
+STATISMO_CORE_EXPORT const static unsigned UNSIGNED_CHAR  = 3;
+STATISMO_CORE_EXPORT const static unsigned SIGNED_SHORT     =  4;
+STATISMO_CORE_EXPORT const static unsigned UNSIGNED_SHORT = 5;
+STATISMO_CORE_EXPORT const static unsigned SIGNED_INT             = 6;
+STATISMO_CORE_EXPORT const static unsigned UNSIGNED_INT   = 7;
+STATISMO_CORE_EXPORT const static unsigned SIGNED_LONG          =  8;
+STATISMO_CORE_EXPORT const static unsigned UNSIGNED_LONG  = 9;
+STATISMO_CORE_EXPORT const static unsigned FLOAT =         10;
+STATISMO_CORE_EXPORT const static unsigned DOUBLE      =   11;
 
 template <class T> unsigned GetDataTypeId() {
 	throw StatisticalModelException("The datatype that was provided is not a valid statismo data type ");
 }
-template <> inline unsigned GetDataTypeId<signed char>() { return SIGNED_CHAR; }
-template <> inline unsigned GetDataTypeId<unsigned char>() { return UNSIGNED_CHAR; }
-template <> inline unsigned GetDataTypeId<signed short>() { return SIGNED_SHORT; }
-template <> inline unsigned GetDataTypeId<unsigned short>() { return UNSIGNED_SHORT; }
-template <> inline unsigned GetDataTypeId<signed int>() { return SIGNED_INT; }
-template <> inline unsigned GetDataTypeId<unsigned int>() { return UNSIGNED_INT; }
-template <> inline unsigned GetDataTypeId<signed long>() { return SIGNED_LONG; }
-template <> inline unsigned GetDataTypeId<unsigned long>() { return UNSIGNED_LONG; }
-template <> inline unsigned GetDataTypeId<float>() { return FLOAT; }
-template <> inline unsigned GetDataTypeId<double>() { return DOUBLE; }
+STATISMO_CORE_EXPORT template <> inline unsigned GetDataTypeId<signed char>() { return SIGNED_CHAR; }
+STATISMO_CORE_EXPORT template <> inline unsigned GetDataTypeId<unsigned char>() { return UNSIGNED_CHAR; }
+STATISMO_CORE_EXPORT template <> inline unsigned GetDataTypeId<signed short>() { return SIGNED_SHORT; }
+STATISMO_CORE_EXPORT template <> inline unsigned GetDataTypeId<unsigned short>() { return UNSIGNED_SHORT; }
+STATISMO_CORE_EXPORT template <> inline unsigned GetDataTypeId<signed int>() { return SIGNED_INT; }
+STATISMO_CORE_EXPORT template <> inline unsigned GetDataTypeId<unsigned int>() { return UNSIGNED_INT; }
+STATISMO_CORE_EXPORT template <> inline unsigned GetDataTypeId<signed long>() { return SIGNED_LONG; }
+STATISMO_CORE_EXPORT template <> inline unsigned GetDataTypeId<unsigned long>() { return UNSIGNED_LONG; }
+STATISMO_CORE_EXPORT template <> inline unsigned GetDataTypeId<float>() { return FLOAT; }
+STATISMO_CORE_EXPORT template <> inline unsigned GetDataTypeId<double>() { return DOUBLE; }
 
 
 } //namespace statismo
